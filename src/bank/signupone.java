36378b1a04379f54c4554688d6f36127c6c5ce6b
khanalbibek <khanalbibek922@gmail.com>
2026-07-16 21:44:46 +0200

    Add files via upload

---
+++ src/bank/signupone.java
@@ -0,0 +1,19 @@
+package bank;
+import javax.swing.*;
+import java.awt.*;
+
+public class signupone extends JFrame {
+    signupone(){
+        getContentPane().setBackground(Color.white);
+        setSize(700, 700);
+        setLocation(350,150);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args){
+        new signupone();
+
+    }
+
+}
